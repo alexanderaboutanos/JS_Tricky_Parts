2f2477ec2d30ba92660e1e360e035a314dd297b3
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2022-03-05 13:57:25 -0500

    completed create account function

--- create-account.js
+++ create-account.js
@@ -1,5 +1,30 @@
-function createAccount(pin, amount) {
+/** @format */
 
+function createAccount(initialPin, amount = 0) {
+  let pin = parseInt(initialPin);
+  let balance = amount;
+  return {
+    checkBalance: function checkBalance(pinAttempt) {
+      return pinAttempt == pin ? `$${balance}` : "Invalid PIN.";
+    },
+    deposit: function deposit(pinAttempt, amt) {
+      if (pinAttempt != pin) return "Invalid PIN.";
+      balance += amt;
+      return `Succesfully deposited $${amt}. Current balance: $${balance}.`;
+    },
+    withdraw: function withdraw(pinAttempt, amt) {
+      if (pinAttempt != pin) return "Invalid PIN.";
+      if (amt > balance)
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      balance -= amt;
+      return `Succesfully withdrew $${amt}. Current balance: $${balance}.`;
+    },
+    changePin: function changePin(pinAttempt, newPin) {
+      if (pinAttempt != pin) return "Invalid PIN.";
+      pin = newPin;
+      return "PIN successfully changed!";
+    },
+  };
 }
 
 module.exports = { createAccount };
